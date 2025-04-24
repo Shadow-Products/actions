a3e6717428bea1510fae63bab7981c8550f4e669
Novickiy Zahar <zahar210200@gmail.com>
2025-04-24 12:24:18 +0300

    add endpoint

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from myproject.views import main_page
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('main/', main_page)
 ]
