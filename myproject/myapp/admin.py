b3540371ca79ffac4ee62bceeaf0027a1084e07c
Novickiy Zahar <zahar210200@gmail.com>
2025-04-24 11:54:13 +0300

    add model3

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from myapp.models import Model1, Model2
+from myapp.models import Model1, Model2, Model3
 
 admin.site.register(Model1)
 admin.site.register(Model2)
+admin.site.register(Model3)
