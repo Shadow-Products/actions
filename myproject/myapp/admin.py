d39f9fb2d905b16b9a82e1f37c16fb34c6df2d3d
EthEnj <112763391+blind1justice@users.noreply.github.com>
2025-04-24 11:55:37 +0300

    add model2 (#2)

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from myapp.models import Model1
+from myapp.models import Model1, Model2
 
 admin.site.register(Model1)
+admin.site.register(Model2)
--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -3,3 +3,7 @@ from django.db import models
 
 class Model1(models.Model):
     field1 = models.CharField(max_length=50)
+
+
+class Model2(models.Model):
+    field = models.CharField(max_length=50)
