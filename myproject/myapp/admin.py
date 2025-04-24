909c0ce76dde71fa331239d3e4961f3813adbc51
Novickiy Zahar <zahar210200@gmail.com>
2025-04-24 15:13:16 +0300

    remove model4

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
-from myapp.models import Model1, Model2, Model3, Model4
+from myapp.models import Model1, Model2, Model3
 
 admin.site.register(Model1)
 admin.site.register(Model2)
 admin.site.register(Model3)
-admin.site.register(Model4)
+
