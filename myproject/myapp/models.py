7e7392d4ae0e1afe994c2e34f759affe8702acaf
Novickiy Zahar <zahar210200@gmail.com>
2025-04-25 13:30:55 +0300

    alter model3

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -7,5 +7,6 @@ class Model2(models.Model):
     field = models.CharField(max_length=50)
 class Model3(models.Model):
     field1 = models.CharField(max_length=50)
+    field2 = models.CharField(max_length=50)
 class Model4(models.Model):
     field1 = models.CharField(max_length=50)    
